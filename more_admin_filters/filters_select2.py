a0b135725d1e97658b983182c8d073999417cc9a
silentbugs <d.misdanitis@gmail.com>
2025-05-06 00:32:10 +0300

    add more select2 simple filters

--- more_admin_filters/filters_select2.py
+++ more_admin_filters/filters_select2.py
@@ -1,9 +1,26 @@
-from .filters import ChoicesDropdownFilter, RelatedDropdownFilter
+from .filters import (
+    ChoicesDropdownFilter,
+    RelatedDropdownFilter,
+    DropdownFilter,
+    RelatedOnlyDropdownFilter,
+)
 
 
-class Select2ChoicesDropdownFilter(ChoicesDropdownFilter):
+class Select2Filter:
     template = "more_admin_filters//select2_related_dropdown_filter.html"
 
 
-class Select2RelatedDropdownFilter(RelatedDropdownFilter):
-    template = "more_admin_filters/select2_related_dropdown_filter.html"
+class Select2DropdownFilter(Select2Filter, DropdownFilter):
+    ...
+
+
+class Select2ChoicesDropdownFilter(Select2Filter, ChoicesDropdownFilter):
+    ...
+
+
+class Select2RelatedDropdownFilter(Select2Filter, RelatedDropdownFilter):
+    ...
+
+
+class Select2RelatedOnlyDropdownFilter(Select2Filter, RelatedOnlyDropdownFilter):
+    ...
